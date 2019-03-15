7ee3185c066240ce71e1ba1483ca0eb5386e40a0
Teodor Lazarov <teodorlazarov@gmail.com>
2019-03-16 00:44:56 +0200

    Initial Project Creation Commit

---
+++ src/main/java/teodorlazarov/getfood/GetFoodProjectApplication.java
@@ -0,0 +1,13 @@
+package teodorlazarov.getfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetFoodProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetFoodProjectApplication.class, args);
+    }
+
+}
